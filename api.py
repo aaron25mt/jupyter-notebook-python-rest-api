63e88af406cc0f6c6d1a35b58e8517ed8ec6d865
Aaron Thomas <aaron25mt@gmail.com>
2019-04-17 12:45:38 -0500

    Update API and add post route

--- api.py
+++ api.py
@@ -1,23 +1,33 @@
 import tornado.ioloop
 import tornado.web
 import subprocess
+from tornado.escape import json_decode
 from notebook_recommender import setup, querySchema
 
 ix = None
 PORT = 8000
 
 class MainHandler(tornado.web.RequestHandler):
-    async def get(self):
+    def output_notebooks(self, me, query):
         if (ix is None):
             return
-        keyword = self.get_argument("keyword", None, True)
-        notebooks = querySchema(ix, keyword)
-        self.write({"notebooks": notebooks})
+        notebooks = querySchema(ix, query)
+        me.write({"notebooks": notebooks})
+
+    async def get(self):
+        keyword = self.get_argument("keyword", "", True)
+        self.output_notebooks(self, keyword)
+
+    async def post(self):
+        body = json_decode(self.request.body)
+        keyword = body["keyword"] if "keyword" in body else ""
+        self.output_notebooks(self, keyword)
 
 def make_app():
     return tornado.web.Application([
         (r"/api/rec/jupyter", MainHandler),
     ])
+
 if __name__ == "__main__":
     app = make_app()
     ix = setup()
